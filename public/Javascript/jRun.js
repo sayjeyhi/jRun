d4794adedb5cf2b9bbaff026a48d404499e972c8
joseph <bomber.man87@yahoo.com>
2018-06-06 21:56:06 +0430

    improve loading mechanism , add a lot of features and blo blo ..

--- public/Javascript/jRun.js
+++ public/Javascript/jRun.js
@@ -2,7 +2,7 @@
  * Main JavaScript file to manage pages js
  *
  * @author   : jafar rezaei [bomber.man87@yahoo.com <http://jrjs.ir>]
- * @updated : 2018/02/20
+ * @updated : 2018/06/06
  */
 var jRun = {
 
@@ -10,32 +10,136 @@ var jRun = {
     /**
      *  jRun version to check updates and force files caching validation
      */
-    version: "0.1.08",
+    version: "0.1.17",
 
 
     /**
      *  Main plugIns that you want to load at page load
      *  @note : add only main plugIns here and use init
-     *        jRun.plugins = [{
-                url : "jquery/jquery.min" ,
-                kind : "PlugIn"
-              }];
+     *     jRun.plugins = [{
+     *        url : "jquery/jquery.min" ,
+     *        kind : "PlugIn"
+     *     }];
      */
     plugins: [],
 
 
+    /**
+     * Set array of types we let them to initialize with .init function
+     */
+    allowedInitTypes : [
+        'css',
+        'js'
+    ],
+
 
     /**
-     * enable system debug mode
+     * If a file has type which not allowed by `allowedInitTypes` Or has
+     * not any type we use js type.
+     */
+    defaultType : 'js',
+
+
+    /**
+     * Default properties for loadFile function
+     */
+    defaultLoadProperties : {
+        wait : false,
+        kind : false,
+        url : '',
+        attributes : {},
+        multiFileUrl : false,
+        afterLoad : function () {},
+        beforeLoad: function() {},
+        errorLoading : function(){}
+    },
+
+    /**
+     * Attributes which should not added by user
+     */
+    notAllowedUserAttributes : [
+        'href',
+        'src'
+    ],
+
+
+    /**
+     * Keep loaded files
+     */
+    loadedFiles: [],
+
+
+    /**
+     * Enable system debug mode
      */
     debugMode: true,
 
+
     /**
-     * allow init files flag , this flag check if main
+     * Allow init files flag , this flag check if main
      * plugIns loaded already or not and then allow init
      * the utility or even plug-ins which user want to add.
      */
     allowInit: false,
+
+
+    /**
+     * Remove extra chars from script name
+     * @param name
+     * @return string
+     */
+    sanitizeName: function (name) {
+        return name.replace(/.min|\/|\.|-/g, "_");
+    },
+
+
+    /**
+     * Set flag to remember which files are loaded
+     * @param name
+     */
+    setLoadedFlag: function (name) {
+        var fileCorrectName = jRun.sanitizeName(name);
+        jRun.loadedFiles.push(fileCorrectName);
+    },
+
+
+    /**
+     * Check if a file is loaded or not
+     * @param script
+     * @returns {boolean}
+     */
+    checkLoad: function (script) {
+        script = jRun.sanitizeName(script);
+        return jRun.loadedFiles.indexOf(script) > -1;
+    },
+
+
+    /**
+     * Extend object
+     *
+     * @param out
+     * @returns {*|{}}
+     */
+    deepExtend: function (out) {
+        out = out || {};
+
+        for (var i = 1; i < arguments.length; i++) {
+            var obj = arguments[i];
+
+            if (!obj)
+                continue;
+
+            for (var key in obj) {
+                if (obj.hasOwnProperty(key)) {
+                    if (typeof obj[key] === 'object')
+                        out[key] = jRun.deepExtend(out[key], obj[key]);
+                    else
+                        out[key] = obj[key];
+                }
+            }
+        }
+        return out;
+    },
 
 
     /**
@@ -45,10 +149,10 @@ var jRun = {
     build: function () {
 
         // alias log instead of console.log in dev mode
-        window.log = function (message) {
-            var e = new Error();
-            var stack = e.stack.toString().split(/\r\n|\n/);
-            console.log(message, '          [' + stack[1] + ']');
+        window.log = function (message , type ) {
+            type = type || 'log';
+            var stack = new Error().stack.toString().split(/\r\n|\n/);
+            console[type](message, '          [' + stack[1] + ']');
         };
 
         // load main configuration
@@ -76,7 +180,6 @@ var jRun = {
      * Add specific file (css,js) to our page
      * @param urls
      * @param callback
-     * @TODO ADD multi URL of a specific kind support
      */
     init: function (urls , callback ) {
 
@@ -89,60 +192,85 @@ var jRun = {
                         return false;
                     return str.indexOf(suffix, str.length - suffix.length) !== -1;
                 },
-                scapeFilename = function (name) {
-                    if (name.indexOf("/") > 0) {
-                        name = name.substr(0, name.indexOf("/"));
-                    }
-                    return name.replace([".min", "/", ".", "-"], "");
-                },
-                loadFinish = function (inOneUrlArray) {
-                    if(!inOneUrlArray)
+                loadFinish = function (o) {
+                    if(!o.multiFileUrl)
                         loadFinishCount++;
+
+                    // call self after load function
+                    o.afterLoad();
 
                     if (urls.length === loadFinishCount) {
                         if (callback !== undefined && typeof callback === "function") {
+                            log("Start All Callback ........");
                             callback();
-                            log("StartCallbak ........")
                         } else {
-                            var callbackError = new Error();
-                            var callbackStack = callbackError.stack.toString().split(/\r\n|\n/);
+                            var callbackStack = new Error().stack.toString().split(/\r\n|\n/);
                             throw "CallBack should be a function " + (typeof callback) + " given!          [" + callbackStack[1] + "]";
                         }
                     }
                 },
 
-                loadFile = function (url, wait, kind, after, inOneUrlArray) {
+                loadFile = function (o) {
+
 
                     if(!waiting) {
 
+                        var realName = jRun.sanitizeName(o.url);
+
+                        // if file is loaded already
+                        if(jRun.checkLoad(realName)){
+                            if(jRun.debugMode){
+                                log(realName + " Was already added !");
+                                log(jRun.loadedFiles);
+                            }
+                            loadFinish(o);
+                            return false;
+                        }
+
                         // add file added flag
-                        var fileCorrectName = scapeFilename(url);
-                        jRun["has" + fileCorrectName] = true;
+                        jRun.setLoadedFlag(realName);
 
-                        kind = kind === "" ? "" : kind + "/";
 
-                        var type = endsWith(url, ".css") ? "css" : "js";
+                        var kind = o.kind ? o.kind + "/" : "";
+                        var type = o.url.split('.').pop();
+
+                        if(jRun.allowedInitTypes.indexOf(type) < 0){
+                            type = jRun.defaultType;
+                        }
+
                         var fileReference = document.createElement((type === "js" ? 'script' : 'link'));
+                        var url = endsWith(o.url , "." + type) ? o.url : o.url + "." + type;
 
-                        url = endsWith(url , "." + type) ? url : url + "." + type;
+                        o.beforeLoad();
+
 
                         if (type === "js") {
-                            fileReference.src = "public/Javascript/" + kind + url + "?Ver=" + jRun.version;
+                            fileReference.src = "public/Javascript/" + kind + url + "?jVer=" + jRun.version;
                         } else {
                             fileReference.type = "text/css";
                             fileReference.rel = "stylesheet";
-                            fileReference.href = "public/Css/" + kind + url + "?Ver=" + jRun.version;
+                            fileReference.href = "public/Css/" + kind + url + "?jVer=" + jRun.version;
                         }
+
+
+                        jRun.notAllowedUserAttributes.forEach(function (attr) {
+                            delete o.attributes[attr];
+                        });
+
+
+                        // add user extra attributes
+                        fileReference = jRun.deepExtend(fileReference , o.attributes);
 
                         fileReference.onload = function(){
                             if (jRun.debugMode) {
-                                log('Loaded script ...' + inOneUrlArray + " - " + url);
+                                log('Loaded script ...' + o.multiFileUrl + " - " + o.url);
                             }
-                            loadFinish(inOneUrlArray);
-                        }; 
+                            loadFinish(o);
+                        };
 
 
                         fileReference.onerror = function () {
+                            o.errorLoading();
                             if (jRun.debugMode) {
                                 log('Error loading script !');
                             }
@@ -152,7 +280,7 @@ var jRun = {
                     }else{
                         // sleep a bit and call your self again after 100 milliSeconds
                         setTimeout(function () {
-                            loadFile(url, wait, after);
+                            loadFile(o);
                         } , 100);
                     }
                 };
@@ -161,32 +289,29 @@ var jRun = {
             urls = (typeof urls === "string") ? [urls] : urls;
 
 
+            urls.forEach(function (url) {
+                log(url);
 
-            for (var i = 0; i < urls.length; i++) {
+                var options = {};
 
-                var address, wait, after, kind = '';
+                options.url = url.hasOwnProperty('url') ? url['url'] : url;
+                options = jRun.deepExtend({}, jRun.defaultLoadProperties , url);
 
-                // is an object and has some dependency
-                wait = urls[i].hasOwnProperty('wait') ? urls[i]['wait'] : undefined;
-                kind = urls[i].hasOwnProperty('kind') ? urls[i]['kind'] : '';
-                after = urls[i].hasOwnProperty('after') ? urls[i]['after'] : undefined;
-                address = urls[i].hasOwnProperty('url') ? urls[i]['url'] : urls[i];
-
-                if(Array.isArray(address)){
-
-                    for (var j = address.length - 1; j >= 0; j--) {
-                        var inOneUrlArray =  (j !== 1);
-                        loadFile(address[j], wait, kind, after , inOneUrlArray);
+                if(Array.isArray(options.url)){
+                    for (var j = options.url.length - 1; j >= 0; j--) {
+                        options.multiFileUrl = (j !== 1);
+                        options.url = options.url[j];
+                        loadFile(options);
                     }
                 }else{
-                    loadFile(address, wait, kind, after , false);
+                    loadFile(options);
                 }
-            }
+            });
 
         }else{
             // sleep a bit and call your self again after 100 milliSeconds
             setTimeout(function () {
-                jRun.init(urls , callback , kind);
+                jRun.init(urls , callback);
             } , 100);
         }
 
@@ -198,11 +323,5 @@ var jRun = {
 // get sass or less  concat them
 // get js files and minify and concat them
 // get pug and transpile it
-/* // Create the event
-var event = new CustomEvent("name-of-event", { "detail": "Example of an event" });
-
-// Dispatch/Trigger/Fire the event
-document.dispatchEvent(event);
-*/
 
 jRun.build();
