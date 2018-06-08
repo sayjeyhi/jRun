f49d99ebd46c8e48e36bc378dd414dea14091539
joseph <bomber.man87@yahoo.com>
2018-06-08 15:17:47 +0430

    versioning bug fix

--- Dist/jRun.js
+++ Dist/jRun.js
@@ -1,8 +1,9 @@
 /**
  * Main JavaScript file to manage pages js
  *
- * @author   : jafar rezaei [bomber.man87@yahoo.com <http://jrjs.ir>]
- * @updated : 2018/06/07
+ * @author    : jafar rezaei <bomber.man87@yahoo.com>
+ * @website   : http://jrjs.ir
+ * @updated   : 2018/06/08
  */
 var jRun = {
 
@@ -10,7 +11,7 @@ var jRun = {
     /**
      *  jRun version to check updates and force files caching validation
      */
-    version: "1.0.18",
+    version: "1.0.19",
 
 
     /**
@@ -156,11 +157,15 @@ var jRun = {
     build: function () {
 
         // alias log instead of console.log in dev mode
-        window.log = function (message , type ) {
+        var log = function (message , type ) {
             type = type || 'log';
             var stack = new Error().stack.toString().split(/\r\n|\n/);
             console[type](message, '          [' + stack[1] + ']');
         };
+
+        if(typeof window !== 'undefined'){
+            window.log = log;
+        }
 
         // load main configuration
         this.firstCall = true;
--- README.md
+++ README.md
@@ -1,4 +1,46 @@
 # jRun
 Main javascript library to manage our js files and handle site creating in a fast and easy
 build passing ...
+
 Demo at : http//jrjs.ir
+
+##Installation
+using npm :
+```bash
+npm i --save jrjs.ir
+```
+in browser :
+```html
+<script type="application/javascript" src="jRun/Dist/jRun.js"></script>
+```
+
+Usage :
+```javascript
+jRun.plugins = [{
+    url : "vivus/vivus.min" ,
+    kind : "PlugIn"
+}];
+jRun.buildPlugIns();
+
+jRun.init(
+    [
+        "javascript1",
+        "path/javascript.min",
+        {
+            url : ['file1.js' , 'file2.js'],
+            attributes : {class : 'myFiles'},
+            multiFileUrl : true,
+            afterLoad : function(){
+                // some code after this part load
+            }
+        },
+        {
+            url: "style.css",
+            kind: "Folder",
+            waitLoading: false
+        }
+    ], function () {
+        // my codes after files load
+    }
+);
+```
